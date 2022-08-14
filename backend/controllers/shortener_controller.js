fa0aa667fa244381b34cb0375c00606706067cbb
suraj <suraj200981@yahoo.co.uk>
2022-08-14 21:17:31 +0200

    regex created for step 1 validation.

--- backend/controllers/shortener_controller.js
+++ backend/controllers/shortener_controller.js
@@ -1,8 +1,15 @@
 exports.urlShortner = (req, res) => {
+
   const url = req.body.url;
-  if (url == "" || url == null || url == undefined || url == " ") {
+
+  //step 1 - check prefix of url
+  //regular expression to check if at least one character is present followed by a dot
+  const regex = /^[a-zA-Z0-9]*\.[a-zA-Z0-9]*$/;
+  if (!regex.test(url)) {
     console.log("invaild url");
-  } else {
-    console.log(url, "is vaild");
-  }
-};
+
+    }else{
+      console.log(url, "is vaild");
+
+    }
+  };

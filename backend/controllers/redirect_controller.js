c8d5d9951be7846312f3e5dd93bbb520688c8e25
suraj200981 <suraj200981@yahoo.co.uk>
2022-08-21 20:14:25 +0200

    redirection feature completed

--- backend/controllers/redirect_controller.js
+++ backend/controllers/redirect_controller.js
@@ -1,16 +1,28 @@
-exports.checkShortUrl = (req, res) => {
-  const gencode = req.params.gencode;
+const mongodbConnection = require("../util/database.js");
+const url = require("url");
 
-  console.log(gencode);
-  //   const db = mongodbConnection.getDB();
-  //   return db
-  //     .collection("orignialurls")
-  //     .findOne({ shortURL: shortUrl })
-  //     .then((result) => {
-  //       console.log(result);
-  //       return result;
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
+exports.checkShortUrl = (req, res) => {
+  const gencode = req.params.generatedCode;
+  const orginalUrl = req.get("host") + req.url;
+  if (gencode.length == 5) {
+    //proceed to check if the code is in the database
+    console.log(gencode, "is a valid code");
+
+    const db = mongodbConnection.getDB();
+    return db
+      .collection("orignialurls")
+      .findOne({ shortURL: orginalUrl })
+      .then((result) => {
+        console.log(result);
+        console.log("Redirecting to: ", result.url);
+        //redirect to external url
+        res.redirect(result.url);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  } else {
+    //404 error
+    console.log(gencode, "incorrect code");
+  }
 };

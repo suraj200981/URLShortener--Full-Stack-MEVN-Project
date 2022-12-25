602996edeb892e41b8650872f37554be8ab3beb6
suraj <suraj200981@yahoo.co.uk>
2022-12-25 20:41:29 +0000

    login redirection happening on frontend

--- backend/app.js
+++ backend/app.js
@@ -16,8 +16,6 @@ const redirectRoute = require("./routes/redirect_route.js");
 const urlDataRoute = require("./routes/urlData_route.js");
 const authRoute = require("./routes/auth_route.js");
 
-app.use(cors());
-//enable cross origin requests
 
 // support parsing of application/json type post data
 app.use(bodyParser.json());
@@ -25,20 +23,9 @@ app.use(bodyParser.json());
 //support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var originsWhitelist = [
-  'http://localhost:8080/'     //this is my front-end url for development
 
-];
-var corsOptions = {
-  origin: function(origin, callback){
-        console.log(origin);
-        var isWhitelisted = originsWhitelist.indexOf(origin) !== -1;
-        console.log(isWhitelisted);
-        callback(null, isWhitelisted);
-  },
-  credentials:true
-}
-app.options('*', cors(corsOptions));
+app.use(cors())//enable cross origin requests
+
 
 
 app.use((req, res, next) => {
@@ -63,7 +50,7 @@ app.use(redirectRoute);
 
 mongoose
   .connect(
-    "mongodb+srv://suraj:Zo6Jkk2oL45tNPby@url-shortener-cluster.gh5rf5b.mongodb.net/urlshortener?retryWrites=true&w=majority"
+    "mongodb+srv://suraj:/@url-shortener-cluster.gh5rf5b.mongodb.net/urlshortener?retryWrites=true&w=majority"
   )
   .then((result) => {
     console.log("Connected to MongoDB");
